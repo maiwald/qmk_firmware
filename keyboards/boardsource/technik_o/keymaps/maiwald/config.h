fdfa20dc22e98884066e100d94e73d443e0cd7a7
Luciano Maiwald <luciano.maiwald@gmail.com>
2021-08-20 21:09:36 +0200

    add technik changes after actually having it :)

---
+++ keyboards/boardsource/technik_o/keymaps/maiwald/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+#define COMBO_COUNT 2
+#define COMBO_TERM 50
