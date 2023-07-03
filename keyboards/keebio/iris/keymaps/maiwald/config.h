87392dfcb35129f5a4ba06292feaa8413e21aad2
Luciano Maiwald <mail@luciano-maiwald.de>
2023-07-03 11:44:28 +0200

    IGNORE_MOD_TAP_INTERRUPT should no longer be set

--- keyboards/keebio/iris/keymaps/maiwald/config.h
+++ keyboards/keebio/iris/keymaps/maiwald/config.h
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM_PER_KEY
 
