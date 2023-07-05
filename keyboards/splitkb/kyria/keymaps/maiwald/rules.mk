b2f1577db2e0f1bd5bbcd388d3862304398d5462
Luciano Maiwald <mail@luciano-maiwald.de>
2023-07-05 09:39:42 +0200

    Add Kyria keymap

---
+++ keyboards/splitkb/kyria/keymaps/maiwald/rules.mk
@@ -0,0 +1,7 @@
+SRC += achordion.c
+
+CONSOLE_ENABLE = yes
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+OLED_ENABLE = no
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
