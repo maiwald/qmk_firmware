f635a94f5b13f84122de54cf0837ebf784a7f587
Luciano Maiwald <mail@luciano-maiwald.de>
2026-07-07 14:39:43 +0200

    Add App tab to Technik

--- keyboards/boardsource/technik_o/keymaps/maiwald/keymap.c
+++ keyboards/boardsource/technik_o/keymaps/maiwald/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  DE_Q,    DE_W,    DE_F,    DE_P,    DE_B,    DE_J,    DE_L,    DE_U,    DE_Y,   DE_MINS, KC_BSPC,
       KC_ESC,  COL_A,   COL_R,   COL_S,   COL_T,   DE_G,    DE_M,    COL_N,   COL_E,   COL_I,  COL_O,   KC_ENT,
       KC_LSFT, DE_Z,    DE_X,    DE_C,    DE_D,    DE_V,    DE_K,    DE_H,    DE_COMM, DE_DOT, MY_SLSH, KC_RSFT,
-      KC_LCTL, XXXXXXX, KC_LALT, KC_LGUI, OSM_SFT, SYMBOLS, SYMBOLS, EXT_SPC, COL_GER, MEDIA,  XXXXXXX, XXXXXXX
+      APP_TAB, XXXXXXX, KC_LALT, KC_LGUI, OSM_SFT, SYMBOLS, SYMBOLS, EXT_SPC, COL_GER, MEDIA,  XXXXXXX, XXXXXXX
       ),
 
   [_PUQ] = LAYOUT_ortho_4x12(
